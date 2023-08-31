2cc7019ed7a6262ec6c4ec97a6cdc44061accec7
Nikolay Petrov <nikip1303@gmail.com>
2023-08-31 13:52:52 +0300

    nav bar created

--- quiz-app/src/components/common/Header/Header.js
+++ quiz-app/src/components/common/Header/Header.js
@@ -1,7 +1,23 @@
+import { Link } from "react-router-dom";
+import style from "./styles/header.module.css"
+
 export const Header = () => {
     return(
         <header>
-            
+            <nav className={style["nav-bar"]}>
+                <h2 >QuizoTopia</h2>
+                <ul className={style["ul-nav"]}>
+                    <li>
+                        <Link to="/quiz">Quizes</Link>
+                    </li>
+                    <li>
+                        <Link to="/">Profile</Link>
+                    </li>
+                    <li><Link to="/">Login</Link></li>
+                    <li><Link to="/">Register</Link></li>
+                    <li><Link to="/">Logout</Link></li>
+                </ul>
+            </nav>
         </header>
     )
 }
