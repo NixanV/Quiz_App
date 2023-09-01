256ab45848e0d277dce63cd2569a1b7d91571fab
Nikolay Petrov <nikip1303@gmail.com>
2023-09-01 15:19:26 +0300

    buttons created in quizes page

--- quiz-app/src/components/Questions/Questions.js
+++ quiz-app/src/components/Questions/Questions.js
@@ -1,8 +1,15 @@
 import * as service from "../../services/quizService"
 import { useEffect, useState } from 'react'
+import styles from "./styles/questions.module.css"
+import { useNavigate } from "react-router-dom"
 
 export const QuizPage = () => {
     const [data, setData] = useState([]);
+    const navigate = useNavigate();
+
+    const NavigateToQuizes = () => {
+        //Todo: navigate to different pages of quizes through buttons
+    }
 
     useEffect(() => {
         service.getAll()
@@ -11,17 +18,45 @@ export const QuizPage = () => {
     }, [])
 
     return(
-        <div>
-        <h1>Trivia Questions</h1>
-        <ul>
-          {data.map((data) => (
-            <li>
-              <h2>{data.question}</h2>
-              <p>Right answer: {data.correct_answer}</p>
-            </li>
-          ))}
-        </ul>
-      </div>
+        <main className={styles["main_section_categories"]}>
+            <section className={styles["section_category"]}>
+                <div className={styles["wrapper_heading"]}>
+                    <h2>Categories</h2>
+                </div>     
+            </section>
+            <section className={styles["types_quizes"]}>
+                <div>
+                    <button>General knowledge</button>
+                </div>
+                <div>
+                    <button>History</button>
+                </div>
+                <div>
+                    <button>Sports</button>
+                </div>
+                <div>
+                    <button>Geography</button>
+                </div>
+                <div>
+                    <button>Animals</button>
+                </div>
+                <div>
+                    <button>Vehicles</button>
+                </div>
+                <div>
+                    <button>Art</button>
+                </div>
+                <div>
+                    <button>Movies</button>
+                </div>
+                <div>
+                    <button>Music</button>
+                </div>
+                <div>
+                    <button>Video Games</button>
+                </div>
+            </section>
+        </main>
     );
     
 }
